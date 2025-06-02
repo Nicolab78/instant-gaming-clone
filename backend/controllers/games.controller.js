3aec672c19dbbe8932d0c1f666c0ed2c04c4625a
Nicolab78 <nicobou60@gmail.com>
2025-06-02 16:14:47 +0200

    Ajout du backend dans Docker

--- backend/controllers/games.controller.js
+++ backend/controllers/games.controller.js
@@ -5,7 +5,7 @@ exports.getAllGames = (req, res) => {
     db.query('SELECT * FROM games', (err, results) => {
         if (err) {
             console.error('Error fetching games:', err);
-            return res.status(500).json({ error: 'Internal server error' });
+            return res.status(500).json({ error: 'Erreur serveur' });
         }
         res.json(results);
     });
